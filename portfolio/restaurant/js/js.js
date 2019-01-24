0c54582e075be89ffa3b7e5e031d49e1f8005adf
isaacgys <46815418+isaacgys@users.noreply.github.com>
2019-01-24 23:37:55 +0800

    Add files via upload

---
+++ portfolio/restaurant/js/js.js
@@ -0,0 +1,29 @@
+// function showText(id){
+	// switch(id) {
+	  // case 1:
+		// document.getElementById('menu1text').style.visibility="visible";
+		// break;
+	  // case 2:
+		// document.getElementById('menu2text').style.visibility="visible";
+		// break;
+	  // case 3:
+		// document.getElementById('menu3text').style.visibility="visible";
+		// break;
+	// } 
+	
+// }
+
+// function hideText(id){
+	// switch(id) {
+	  // case 1:
+		// document.getElementById('menu1text').style.visibility="hidden";
+		// break;
+	  // case 2:
+		// document.getElementById('menu2text').style.visibility="hidden";
+		// break;
+	  // case 3:
+		// document.getElementById('menu3text').style.visibility="hidden";
+		// break;
+	// } 
+	
+// }
